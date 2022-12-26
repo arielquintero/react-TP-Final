890f65d2fcb0bb5cfa8add0ea9d22dda926930cd
arielquintero <ariel8777@yahoo.com.ar>
2022-12-26 12:08:11 -0300

    arreglado!

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import ItemListContainer from "./ComponentContainer/ItemDetailContainer/ItemDetailContainer";
+// TODO: Prestar atencion a las importaciones
+import ItemListContainer from "./ComponentContainer/ItemListContainer";
 import ItemDetailContainer from "./ComponentContainer/ItemDetailContainer/ItemDetailContainer";
 import { CartContextProvider } from "./context/CartContext";
 import CartContainer from "./ComponentContainer/CartContainer/CartContainer";
 import "./App.scss";
 
 function App() {
-    // const showName = (name) => name;
-    return (
-        <BrowserRouter>
-            <CartContextProvider value={{}}>
-                <NavBar />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <ItemListContainer
-                                // greeting={"Te saludo desde Item List Container, "}
-                                // myName={showName("Ariel")}
-                            />
-                        }
-                    />
-                    <Route
-                        path="/category/:idCategory"
-                        element={<ItemListContainer />}
-                    />
-                    <Route
-                        path="/detail/:productId"
-                        element={<ItemDetailContainer />}
-                    />
-                    {/* <Route path="/cart" element={<CartContainer />} /> */}
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-            </CartContextProvider>
-        </BrowserRouter>
-    );
+	// const showName = (name) => name;
+	return (
+		<BrowserRouter>
+			<CartContextProvider>
+				<NavBar />
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<ItemListContainer
+							// greeting={"Te saludo desde Item List Container, "}
+							// myName={showName("Ariel")}
+							/>
+						}
+					/>
+					<Route
+						path="/category/:idCategory"
+						element={<ItemListContainer />}
+					/>
+					<Route
+						path="/detail/:productId"
+						element={<ItemDetailContainer />}
+					/>
+					<Route path="/cart" element={<CartContainer />} />
+					<Route path="*" element={<Navigate to="/" />} />
+				</Routes>
+			</CartContextProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/ComponentContainer/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/ComponentContainer/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,27 +1,26 @@
-import { doc, getDoc, getFirestore } from "firebase/firestore";
-// import { getDocFirestore } from "../../firestore/getsFirestore";
-import { useState, useEffect } from "react";
+// import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { getDocFirestore } from "../../firestore/getsFirestore";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-// import { useCartContext } from "../../context/CartContext";
+import { useCartContext } from "../../context/CartContext";
 import ItemDetail from "../../Components/ItemDetail/ItemDetail";
-// import Loading from "../../Components/Loading/Loading";
+import Loading from "../../Components/Loading/Loading";
 
 const ItemDetailContainer = () => {
 	const [product, setProduct] = useState({});
-	// const { isLoading, setIsLoading } = useCartContext();
+	const { isLoading, setIsLoading } = useCartContext();
 	const { productId } = useParams();
-		// getDocFirestore(productId, setProduct, setIsLoading);
 
-	 useEffect(() => {
-        const db = getFirestore();
-        const queryDoc = doc(db, "productos", productId);
-        getDoc(queryDoc).
-        then((data) => setProduct({ id: data.id, ...data.data() }));
-    }, [productId]);
+	getDocFirestore(productId, setProduct, setIsLoading);
 
+	//  useEffect(() => {
+	//     const db = getFirestore();
+	//     const queryDoc = doc(db, "productos", productId);
+	//     getDoc(queryDoc).
+	//     then((data) => setProduct({ id: data.id, ...data.data() }));
+	// }, [productId]);
 
-    return <ItemDetail product={product} />;
-    // isLoading ? <Loading /> : <ItemDetail product={product} /> ;
+	return isLoading ? <Loading /> : <ItemDetail product={product} />;
 };
 
 export default ItemDetailContainer;
